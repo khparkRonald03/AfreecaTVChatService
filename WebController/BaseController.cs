919ecea4ae0819440f7f148808730a6b48601449
ronald03 <ronald0350044@gmail.com>
2018-06-28 03:12:01 +0900

    셀레니움 설정변경

--- WebController/BaseController.cs
+++ WebController/BaseController.cs
@@ -39,18 +39,18 @@ namespace WebController
                 Options.AddArgument("headless");
                 Options.AddArgument("mute-audio");
                 Options.AddArgument("--start-maximized");
-                Options.AddArguments("user-data-dir=/path/to/your/custom/profile");
+                //Options.AddArguments("user-data-dir=/path/to/your/custom/profile");
 
                 string adlockPath = $"{startupPath}\\Adblock-Plus_v3.1.crx";
                 //string vpnPath = $"{startupPath}\\Unlimited-Free-VPN-Hola_v1.99.348.crx";
-                string vpnPath = $"{startupPath}\\UltraSurf-Security,-Privacy-&-Unblock-VPN_v1.5.4.crx";
+                //string vpnPath = $"{startupPath}\\UltraSurf-Security,-Privacy-&-Unblock-VPN_v1.5.4.crx";
                 string[] extentionparams = new string[2];
 
                 if (File.Exists(adlockPath))
                     extentionparams[0] = adlockPath;
 
-                if (File.Exists(vpnPath))
-                    extentionparams[1] = vpnPath;
+                //if (File.Exists(vpnPath))
+                //    extentionparams[1] = vpnPath;
 
                 if (extentionparams.Length > 0)
                     Options.AddExtensions(extentionparams);
